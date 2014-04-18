5c7dd5157bb34dda6490704b5c7247c49b5db577
MrInspectorDev <themrinspector@yahoo.com>
2014-04-18 10:32:06 -0700

    Update MPK.java

--- src/com/mrinspector/plugin/MPK.java
+++ src/com/mrinspector/plugin/MPK.java
@@ -1,9 +1,15 @@
 package com.mrinspector.plugin;
 
+import java.io.IOException;
+
 import net.milkbowl.vault.economy.Economy;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Animals;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
@@ -11,13 +17,25 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.mcstats.Metrics;
+
+import com.ricbyte.api.API;
 
 public class MPK extends JavaPlugin implements Listener {
 
 	public void onEnable() {
 		Bukkit.getPluginManager().registerEvents(this, this);
+		
 	    getConfig().options().copyDefaults(true);
 	    saveDefaultConfig();
+	    
+		try {
+			Metrics metrics = new Metrics(this);
+			metrics.start();
+		} catch (IOException e) {
+			// Failed to submit the stats :-(
+		}
+	    
 		if (!setupEconomy()) {
 			getLogger().severe(String.format("[%s] - Disabled due to no Vault dependency found!", getDescription().getName()));
 			getServer().getPluginManager().disablePlugin(this);
@@ -42,64 +60,98 @@ public class MPK extends JavaPlugin implements Listener {
 		return eco != null;
 	}
 	
+	  public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		  final Player p = (Player)sender;
+	    	if(cmd.getName().equalsIgnoreCase("mpkreload")) {
+	            super.reloadConfig();
+	            p.sendMessage(ChatColor.GOLD + "[MPK] " + ChatColor.GRAY + "You have reloaded config.");
+	            
+	    	}
+			return false;
+	  }
+	
 	@EventHandler
-	public void onJoinMessage(PlayerJoinEvent e) {
-		e.getPlayer().sendMessage(ChatColor.GOLD + "[MPK]" + ChatColor.GREEN + " Running MPK Version 3.0.0 by" + ChatColor.GRAY + " MrInspector");
+	public void onJoinMessage(PlayerJoinEvent e){
+		if(e.getPlayer().hasPermission("mpk.normal")) {
 		String messageonjoin = super.getConfig().getString("MessageOnJoin");
 		messageonjoin = ChatColor.translateAlternateColorCodes('&', messageonjoin);
 		e.getPlayer().sendMessage(messageonjoin);
 		
-		@SuppressWarnings("unused")
-		boolean disable = super.getConfig().getBoolean("Disable");
-
-		if(super.getConfig().getBoolean("Disable") == true) {
-			Bukkit.getPluginManager().disablePlugin(this);
-			Bukkit.broadcastMessage(ChatColor.RED + "MPK has been disabled!");
+		if(e.getPlayer().hasPermission("mpk.vip")) {
+			String vipmessageonjoin = super.getConfig().getString("VIPMessageOnJoin");
+			vipmessageonjoin = ChatColor.translateAlternateColorCodes('&', vipmessageonjoin);
+			e.getPlayer().sendMessage(vipmessageonjoin);
 		}
+		}
+	}
+	
+	@EventHandler
+	public void onQuit(PlayerQuitEvent e) {
+		e.getPlayer().sendMessage(ChatColor.GOLD + "[MPK] " + ChatColor.GREEN + "Running MPK version 3.1.0 by" + ChatColor.GRAY + "MrInspector.");
+		if(e.getPlayer().hasPermission("mpk.normal")) {
+		String messageonleave = super.getConfig().getString("MessageOnLeave").replace("{name}", e.getPlayer().getName());
+		messageonleave = ChatColor.translateAlternateColorCodes('&', messageonleave);
+		Bukkit.broadcastMessage(messageonleave);
 		
-		if(super.getConfig().getBoolean("Disable") == false) {
-			Bukkit.getPluginManager().enablePlugin(this);
-			Bukkit.broadcastMessage(ChatColor.GREEN + "MPK has been enabled!");
+		if(e.getPlayer().hasPermission("mpk.vip")) {
+			String vipmessageonleave = super.getConfig().getString("VIPMessageOnLeave");
+			vipmessageonleave = ChatColor.translateAlternateColorCodes('&', vipmessageonleave).replace("{name}", e.getPlayer().getName());
+			Bukkit.broadcastMessage(vipmessageonleave);
+		}
+	}
+}
+	
+	@EventHandler
+	public void onPvE(PlayerDeathEvent e) {
+		if(e.getEntity() instanceof Animals) {
+			if(e.getEntity().getKiller() instanceof Player) {
+				String entitykillmsg = super.getConfig().getString("EntityKillMessage").replace("{killersname}", e.getEntity().getPlayer().getKiller().getName().replace("{mob}", e.getEntity().getType().toString()));
+						
+			    double amountonentitykill = super.getConfig().getDouble("AmountOnEntityKill");
+										
+			    eco.depositPlayer(e.getEntity().getKiller().getName(), amountonentitykill);
+				
+				e.getEntity().getKiller().sendMessage(entitykillmsg);
+			}
+				
 		}
 	}
 	
 	@EventHandler
-	public void onQuit(PlayerQuitEvent e){
-		// Do nothing currently
-		// may be used for a leave message
-	}
-	
-	@EventHandler
-	public void onKill(PlayerDeathEvent e) {
-		if(e.getEntity().getPlayer().getKiller().hasPermission("mpk.receivefunds")){
-		String messageonkill = super.getConfig().getString("MessageOnKill");
-		String messageondeath = super.getConfig().getString("MessageOnDeath");
+	public void onKillAndDeath(PlayerDeathEvent e) {
+		if(e.getEntity().getPlayer().getKiller().hasPermission("mpk.normal")){	
+		String messageonkill = super.getConfig().getString("MessageOnKill").replace("{killersname}", e.getEntity().getPlayer().getKiller().getName()).replace("{name}", e.getEntity().getPlayer().getName());
+		String messageondeath = super.getConfig().getString("MessageOnDeath").replace("%player%", e.getEntity().getPlayer().getName());
 		
 		double amountonkill = super.getConfig().getDouble("AmountOnKill");
 		double amountondeath = super.getConfig().getDouble("AmountOnDeath");
 		
-		if(messageonkill.contains("{killersname}")) {
-			messageonkill.replace("{killersname}", e.getEntity().getPlayer().getKiller().getName());
-		}
+		double healthonkill = super.getConfig().getDouble("HealthOnKill");
+		double maxhealthonkill = super.getConfig().getDouble("MaxHealthOnKill");
 		
-		if(messageonkill.contains("{name}")) {
-			messageonkill.replace("{name}", e.getEntity().getPlayer().getName());
-		}
-		
-		if(messageondeath.contains("{killersname}")) {
-			messageondeath.replace("{killersname}", e.getEntity().getPlayer().getKiller().getName()); 
-		}
-		
-		if(messageondeath.contains("{name}")) {
-			messageondeath.replace("{name}", e.getEntity().getPlayer().getName());
-		}
-	   				
-		messageonkill = ChatColor.translateAlternateColorCodes('&', messageonkill);
-		messageondeath = ChatColor.translateAlternateColorCodes('&', messageondeath);
 		e.getEntity().getPlayer().getKiller().sendMessage(messageonkill);
 		e.getEntity().getPlayer().sendMessage(messageondeath);
+		API.addHealth(e.getEntity().getPlayer().getKiller(), (int) healthonkill);
+		API.setMaxHealth(e.getEntity().getPlayer().getKiller(), (int) maxhealthonkill);
 		eco.depositPlayer(e.getEntity().getPlayer().getKiller().getName(), amountonkill);
 		eco.withdrawPlayer(e.getEntity().getPlayer().getName(), amountondeath);
-	}
+		
+		if(e.getEntity().getPlayer().getKiller().hasPermission("mpk.vip")){
+			String vipmessageonkill = super.getConfig().getString("VIPMessageOnKill").replace("{killersname}", e.getEntity().getPlayer().getKiller().getName()).replace("{name}", e.getEntity().getPlayer().getName());
+			String vipmessageondeath = super.getConfig().getString("VIPMessageOnDeath").replace("%player%", e.getEntity().getPlayer().getName());
+			
+			double vipamountonkill = super.getConfig().getDouble("VIPAmountOnKill");
+			double vipamountondeath = super.getConfig().getDouble("VIPAmountOnDeath");
+			double viphealthonkill = super.getConfig().getDouble("VIPHealthOnKill");
+			double vipmaxhealthonkill = super.getConfig().getDouble("VIPMaxHealthOnKill");
+			API.addHealth(e.getEntity().getPlayer().getKiller(), (int) viphealthonkill);
+			API.setMaxHealth(e.getEntity().getPlayer().getKiller(), (int) vipmaxhealthonkill);
+			e.getEntity().getPlayer().getKiller().sendMessage(vipmessageonkill);
+			e.getEntity().getPlayer().sendMessage(vipmessageondeath);
+			eco.depositPlayer(e.getEntity().getPlayer().getKiller().getName(), vipamountonkill);
+			eco.withdrawPlayer(e.getEntity().getPlayer().getName(), vipamountondeath);
+		}
+		
+		}
 	}
 }
